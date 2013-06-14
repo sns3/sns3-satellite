8470de358bb68afde169c4c4982fa13ba2d72f73
Budiarto Herman <budiarto.herman@magister.fi>
2013-06-14 08:42:01 +0300

    SatLinkResults for DVB-S2 and DVB-RCS2

--- model/satellite-link-results.cc
+++ model/satellite-link-results.cc
@@ -103,6 +103,29 @@ SatLinkResultsDvbRcs2::GetTypeId ()
 void
 SatLinkResultsDvbRcs2::DoInitialize ()
 {
+  m_table.resize (21);
+
+  m_table[0] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat2.txt");
+  m_table[1] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat3.txt");
+  m_table[2] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat4.txt");
+  m_table[3] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat5.txt");
+  m_table[4] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat6.txt");
+  m_table[5] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat7.txt");
+  m_table[6] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat8.txt");
+  m_table[7] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat9.txt");
+  m_table[8] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat10.txt");
+  m_table[9] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat11.txt");
+  m_table[10] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat12.txt");
+  m_table[11] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat13.txt");
+  m_table[12] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat14.txt");
+  m_table[13] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat15.txt");
+  m_table[14] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat16.txt");
+  m_table[15] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat17.txt");
+  m_table[16] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat18.txt");
+  m_table[17] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat19.txt");
+  m_table[18] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat20.txt");
+  m_table[19] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat21.txt");
+  m_table[20] = CreateObject<SatLookUpTable> (m_inputPath + "rcs2_wafeformat22.txt");
 
 } // end of void SatLinkResultsDvbRcs2::DoInitialize
 
@@ -111,7 +134,111 @@ uint8_t
 SatLinkResultsDvbRcs2::DoGetTableIndex (SatLinkResults::SatModcod_e modcod,
                                         int16_t burstLength) const
 {
-  return 0;
+  uint8_t waveformId = 0;
+
+  switch (burstLength)
+  {
+    case 262:
+      if (modcod == SAT_MODCOD_QPSK_1_TO_3)
+        {
+          waveformId = 2;
+        }
+      break;
+
+    case 536:
+      switch (modcod)
+      {
+        case SAT_MODCOD_QPSK_1_TO_3:
+          waveformId = 3;
+          break;
+        case SAT_MODCOD_QPSK_1_TO_2:
+          waveformId = 4;
+          break;
+        case SAT_MODCOD_QPSK_2_TO_3:
+          waveformId = 5;
+          break;
+        case SAT_MODCOD_QPSK_3_TO_4:
+          waveformId = 6;
+          break;
+        case SAT_MODCOD_QPSK_5_TO_6:
+          waveformId = 7;
+          break;
+        case SAT_MODCOD_8PSK_2_TO_3:
+          waveformId = 8;
+          break;
+        case SAT_MODCOD_8PSK_3_TO_4:
+          waveformId = 9;
+          break;
+        case SAT_MODCOD_8PSK_5_TO_6:
+          waveformId = 10;
+          break;
+        case SAT_MODCOD_16QAM_3_TO_4:
+          waveformId = 11;
+          break;
+        case SAT_MODCOD_16QAM_5_TO_6:
+          waveformId = 12;
+          break;
+        default:
+          NS_FATAL_ERROR ("Invalid Modcod for DVB-RCS2: " << modcod);
+          break;
+      } // end of switch (modcod)
+
+      break; // end of case 536
+
+    case 1616:
+      switch (modcod)
+      {
+        case SAT_MODCOD_QPSK_1_TO_3:
+          waveformId = 13;
+          break;
+        case SAT_MODCOD_QPSK_1_TO_2:
+          waveformId = 14;
+          break;
+        case SAT_MODCOD_QPSK_2_TO_3:
+          waveformId = 15;
+          break;
+        case SAT_MODCOD_QPSK_3_TO_4:
+          waveformId = 16;
+          break;
+        case SAT_MODCOD_QPSK_5_TO_6:
+          waveformId = 17;
+          break;
+        case SAT_MODCOD_8PSK_2_TO_3:
+          waveformId = 18;
+          break;
+        case SAT_MODCOD_8PSK_3_TO_4:
+          waveformId = 19;
+          break;
+        case SAT_MODCOD_8PSK_5_TO_6:
+          waveformId = 20;
+          break;
+        case SAT_MODCOD_16QAM_3_TO_4:
+          waveformId = 21;
+          break;
+        case SAT_MODCOD_16QAM_5_TO_6:
+          waveformId = 22;
+          break;
+        default:
+          NS_FATAL_ERROR ("Invalid Modcod for DVB-RCS2: " << modcod);
+          break;
+      } // end of switch (modcod)
+
+      break; // end of case 1616
+
+    default:
+      NS_FATAL_ERROR ("Invalid burst length for DVB-RCS2: " << burstLength);
+      break;
+
+  } // end of switch (burstLength)
+
+  if (waveformId == 0)
+    {
+      NS_FATAL_ERROR ("Invalid Modcod/burst length for DVB-RCS2: "
+                      << modcod << "/" << burstLength);
+    }
+
+  return (waveformId - 2);
+
 } // end of uint8_t SatLinkResultsDvbRcs2::DoGetTableIndex
 
 
@@ -181,7 +308,7 @@ uint8_t
 SatLinkResultsDvbS2::DoGetTableIndex (SatLinkResults::SatModcod_e modcod,
                                       uint16_t burstLength) const
 {
-  uint8_t ret = 0;
+  uint8_t ret = 255;
   switch (modcod)
   {
     // QPSK
@@ -271,7 +398,8 @@ SatLinkResultsDvbS2::DoGetTableIndex (SatLinkResults::SatModcod_e modcod,
     default:
       NS_FATAL_ERROR ("Invalid Modcod for DVB-S2: " << modcod);
       break;
-  }
+
+  } // end of switch (modcod)
 
   return ret;
 
