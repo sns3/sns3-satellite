c6c16806f9fd40d8d55fa48972e7e8b7103e8a7e
Jani Puttonen <jani.puttonen@magister.fi>
2013-11-18 11:30:23 +0200

    Changed satellite antenna gain test case to use the default GW positions.

--- test/satellite-antenna-pattern-test.cc
+++ test/satellite-antenna-pattern-test.cc
@@ -61,30 +61,30 @@ SatAntennaPatternTestCase::DoRun (void)
 
   // Test positions (= GW positions from 72 spot-beam reference system)
   std::vector<GeoCoordinate> coordinates;
-  GeoCoordinate g1 = GeoCoordinate(51.50, -0.13, 0.0);
+  GeoCoordinate g1 = GeoCoordinate(50.25, 3.75, 0.0);
   coordinates.push_back (g1);
-  GeoCoordinate g2 = GeoCoordinate(59.91, 10.75, 0.0);
+  GeoCoordinate g2 = GeoCoordinate(64.00, 8.25, 0.0);
   coordinates.push_back (g2);
-  GeoCoordinate g3 = GeoCoordinate(40.42, -3.70, 0.0);
+  GeoCoordinate g3 = GeoCoordinate(42.25, -4.50, 0.0);
   coordinates.push_back (g3);
-  GeoCoordinate g4 = GeoCoordinate(41.90, 12.48, 0.0);
+  GeoCoordinate g4 = GeoCoordinate(44.50, 13.50, 0.0);
   coordinates.push_back (g4);
-  GeoCoordinate g5 = GeoCoordinate(37.98, 23.73, 0.0);
+  GeoCoordinate g5 = GeoCoordinate(37.25, 23.75, 0.0);
   coordinates.push_back (g5);
 
   /* Reference results
-  GW: 1, Lat: 51.5, Lon: -0.13, bestBeamId: 12, gain: 48.9788
-  GW: 2, Lat: 59.91, Lon: 10.75, bestBeamId: 6, gain: 49.2983
-  GW: 3, Lat: 40.42, Lon: -3.7, bestBeamId: 22, gain: 49.1886
-  GW: 4, Lat: 41.9, Lon: 12.48, bestBeamId: 46, gain: 48.8911
-  GW: 5, Lat: 37.98, Lon: 23.73, bestBeamId: 58, gain: 51.0815
+  GW: 1, Lat: 50.25, Lon: 3.75, bestBeamId: 12, gain: 51.5528
+  GW: 2, Lat: 64, Lon: 8.25, bestBeamId: 6, gain: 51.4735
+  GW: 3, Lat: 42.25, Lon: -4.5, bestBeamId: 22, gain: 51.3403
+  GW: 4, Lat: 44.5, Lon: 13.5, bestBeamId: 39, gain: 51.6553
+  GW: 5, Lat: 37.25, Lon: 23.75, bestBeamId: 58, gain: 51.4738
   */
 
   // Expected spot-beam gains from beam 3
-  double expectedGains[5] = { 48.9788, 49.2983, 49.1886, 48.8911, 51.0815 };
+  double expectedGains[5] = { 51.5528, 51.4735, 51.3403, 51.6553, 51.4738 };
 
   // Expected best spot-beams
-  uint32_t expectedBeamIds[5] = {12, 6, 22, 46, 58};
+  uint32_t expectedBeamIds[5] = {12, 6, 22, 39, 58};
 
   // Check that the gains and best beam IDs are as expected
   double gain (0.0);
