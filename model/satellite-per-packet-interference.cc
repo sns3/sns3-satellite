e0a6fbc01741bbf36268dce546a287a2860a3b53
Frans Laakso <frans.laakso@magister.fi>
2013-11-25 13:33:58 +0200

    Disabled one assert as the feature is not yet fully integrated

--- model/satellite-per-packet-interference.cc
+++ model/satellite-per-packet-interference.cc
@@ -54,7 +54,7 @@ SatPerPacketInterference::SatPerPacketInterference ()
     m_channelType (),
     m_rxBandwidth_Hz ()
 {
-  NS_ASSERT(0);
+  //NS_ASSERT(0);
 }
 
 SatPerPacketInterference::SatPerPacketInterference (Ptr<SatInterferenceOutputTraceContainer> traceContainer, SatEnums::ChannelType_t channeltype, double rxBandwidth)
