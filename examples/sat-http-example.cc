3051401d085c33d7efa4c6d3566b2a7ce392c9f5
Budiarto Herman <budiarto.herman@magister.fi>
2013-12-05 00:21:00 +0200

    HTTP KPI helper

--- examples/sat-http-example.cc
+++ examples/sat-http-example.cc
@@ -32,148 +32,6 @@ using namespace ns3;
 NS_LOG_COMPONENT_DEFINE ("SatHttpExample");
 
 
-class HttpCalculator
-{
-public:
-  HttpCalculator (ApplicationContainer apps, Time duration);
-  void Print ();
-private:
-  void RxCallback (std::string context, Ptr<const Packet> packet);
-  void RxMainObjectCallback (std::string context);
-  void RxEmbeddedObjectCallback (std::string context);
-  static std::string GetContext (uint32_t appId);
-  static uint32_t GetAppId (std::string context);
-  Time m_duration;
-  std::vector<uint64_t> m_rxBytes;
-  std::vector<uint32_t> m_rxMainObject;
-  std::vector<uint32_t> m_rxEmbeddedObject;
-};
-
-HttpCalculator::HttpCalculator (ApplicationContainer apps, Time duration)
-  : m_duration (duration)
-{
-  uint32_t n = apps.GetN ();
-  m_rxBytes.resize (n, 0);
-  m_rxMainObject.resize (n, 0);
-  m_rxEmbeddedObject.resize (n, 0);
-
-  uint32_t appId = 0;
-
-  for (ApplicationContainer::Iterator it = apps.Begin ();
-       it != apps.End (); it++)
-    {
-      NS_ASSERT (appId < n);
-      std::string context = GetContext (appId);
-      (*it)->TraceConnect ("RxMainObjectPacket", context,
-                           MakeCallback (&HttpCalculator::RxCallback, this));
-      (*it)->TraceConnect ("RxEmbeddedObjectPacket", context,
-                           MakeCallback (&HttpCalculator::RxCallback, this));
-      (*it)->TraceConnect ("RxMainObject", context,
-                           MakeCallback (&HttpCalculator::RxMainObjectCallback,
-                                         this));
-      (*it)->TraceConnect ("RxEmbeddedObject", context,
-                           MakeCallback (&HttpCalculator::RxEmbeddedObjectCallback,
-                                         this));
-      appId++;
-    }
-}
-
-void
-HttpCalculator::Print ()
-{
-  uint64_t sumRxBytes = 0;
-  uint32_t sumRxMainObject = 0;
-  uint32_t sumRxEmbeddedObject = 0;
-  double throughput = 0.0;
-  double duration = m_duration.GetSeconds ();
-  uint32_t n = m_rxBytes.size ();
-  NS_ASSERT (m_rxMainObject.size () == n);
-  NS_ASSERT (m_rxEmbeddedObject.size () == n);
-
-  NS_LOG_INFO (this << " HTTP clients round-up statistics:");
-  NS_LOG_INFO (this << " -----------------------------------------------------");
-  NS_LOG_INFO (this << std::setw (5) << "#"
-                    << std::setw (12) << "main"
-                    << std::setw (12) << "emb"
-                    << std::setw (12) << "bytes"
-                    << std::setw (12) << "kbps");
-  NS_LOG_INFO (this << " -----------------------------------------------------");
-
-  for (uint32_t i = 0; i < n; i++)
-    {
-      throughput = static_cast<double> (m_rxBytes[i] * 8) / 1000.0 / duration;
-      NS_LOG_INFO (this << std::setw (5) << i
-                        << std::setw (12) << m_rxMainObject[i]
-                        << std::setw (12) << m_rxEmbeddedObject[i]
-                        << std::setw (12) << m_rxBytes[i]
-                        << std::setw (12) << throughput);
-      sumRxBytes += m_rxBytes[i];
-      sumRxMainObject += m_rxMainObject[i];
-      sumRxEmbeddedObject += m_rxEmbeddedObject[i];
-    }
-
-  throughput = static_cast<double> (sumRxBytes * 8) / 1000.0 / duration;
-  NS_LOG_INFO (this << " -----------------------------------------------------");
-  NS_LOG_INFO (this << std::setw (5) << "sum"
-                    << std::setw (12) << sumRxMainObject
-                    << std::setw (12) << sumRxEmbeddedObject
-                    << std::setw (12) << sumRxBytes
-                    << std::setw (12) << throughput);
-  NS_LOG_INFO (this << std::setw (5) << "avg"
-                    << std::setw (12) << static_cast<double> (sumRxMainObject) / n
-                    << std::setw (12) << static_cast<double> (sumRxEmbeddedObject) / n
-                    << std::setw (12) << static_cast<double> (sumRxBytes) / n
-                    << std::setw (12) << static_cast<double> (throughput) / n);
-  NS_LOG_INFO (this << " -----------------------------------------------------");
-
-  NS_UNUSED (throughput);
-}
-
-void
-HttpCalculator::RxCallback (std::string context, Ptr<const Packet> packet)
-{
-  uint32_t appId = GetAppId (context);
-  NS_ASSERT (appId < m_rxBytes.size ());
-  m_rxBytes[appId] += packet->GetSize ();
-}
-
-void
-HttpCalculator::RxMainObjectCallback (std::string context)
-{
-  uint32_t appId = GetAppId (context);
-  NS_ASSERT (appId < m_rxMainObject.size ());
-  m_rxMainObject[appId]++;
-}
-
-void
-HttpCalculator::RxEmbeddedObjectCallback (std::string context)
-{
-  uint32_t appId = GetAppId (context);
-  NS_ASSERT (appId < m_rxEmbeddedObject.size ());
-  m_rxEmbeddedObject[appId]++;
-}
-
-std::string
-HttpCalculator::GetContext (uint32_t appId)
-{
-  std::ostringstream context;
-  context << appId;
-  return context.str ();
-}
-
-uint32_t
-HttpCalculator::GetAppId (std::string context)
-{
-  std::stringstream ss (context);
-  uint32_t appId;
-  if (!(ss >> appId))
-    {
-      NS_FATAL_ERROR ("Cannot convert context '" << context << "' to number");
-    }
-  return appId;
-}
-
-
 /**
  * \ingroup satellite
  *
@@ -260,8 +118,8 @@ main (int argc, char *argv[])
   httpHelper.GetServer ().Start (Seconds (1.0));
   httpHelper.GetClients ().Start (Seconds (3.0));
 
-  // install calculator
-  HttpCalculator httpCalculator (httpHelper.GetClients (), Seconds (duration));
+  // install KPI statistics collector
+  HttpKpiHelper kpiHelper (&httpHelper);
 
   NS_LOG_INFO ("--- sat-http-example ---");
   NS_LOG_INFO ("  Scenario used: " << scenario);
@@ -271,7 +129,7 @@ main (int argc, char *argv[])
   Simulator::Stop (Seconds (duration));
   Simulator::Run ();
 
-  httpCalculator.Print ();
+  kpiHelper.Print ();
 
   Simulator::Destroy ();
 
