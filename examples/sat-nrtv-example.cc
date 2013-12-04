aa9706e48201e0a314b9206cec0b90c6e9854220
Budiarto Herman <budiarto.herman@magister.fi>
2013-12-04 16:30:30 +0200

    Reverted NRTV client from reading packet headers and added KPI helper

--- examples/sat-nrtv-example.cc
+++ examples/sat-nrtv-example.cc
@@ -24,125 +24,11 @@
 #include <ns3/internet-module.h>
 #include <ns3/satellite-module.h>
 #include <ns3/traffic-module.h>
-#include <iomanip>
 
 using namespace ns3;
 
 
 NS_LOG_COMPONENT_DEFINE ("SatNrtvExample");
-
-
-class NrtvCalculator
-{
-public:
-  NrtvCalculator (ApplicationContainer apps, Time duration);
-  void Print ();
-private:
-  void RxCallback (std::string context, Ptr<const Packet> packet);
-  static std::string GetContext (uint32_t appId);
-  static uint32_t GetAppId (std::string context);
-  ApplicationContainer m_apps;
-  Time m_duration;
-  std::vector<uint64_t> m_rxBytes;
-};
-
-NrtvCalculator::NrtvCalculator (ApplicationContainer apps, Time duration)
-  : m_apps (apps),
-    m_duration (duration)
-{
-  uint32_t n = apps.GetN ();
-  m_rxBytes.resize (n, 0);
-
-  uint32_t appId = 0;
-
-  for (ApplicationContainer::Iterator it = apps.Begin ();
-       it != apps.End (); it++)
-    {
-      NS_ASSERT ((*it)->GetObject<NrtvClient> () != 0);
-      NS_ASSERT (appId < n);
-      std::string context = GetContext (appId);
-      (*it)->TraceConnect ("Rx", context,
-                           MakeCallback (&NrtvCalculator::RxCallback, this));
-      appId++;
-    }
-}
-
-void
-NrtvCalculator::Print ()
-{
-  uint64_t sumRxBytes = 0;
-  double throughput = 0.0;
-  uint32_t sumRxPackets = 0;
-  Time sumRxDelay = MilliSeconds (0);
-  Time delay = MilliSeconds (0);
-  double duration = m_duration.GetSeconds ();
-  uint32_t n = m_rxBytes.size ();
-
-  NS_LOG_INFO (this << " NRTV clients round-up statistics:");
-  NS_LOG_INFO (this << " -----------------------------------------");
-  NS_LOG_INFO (this << std::setw (5) << "#"
-                    << std::setw (12) << "bytes"
-                    << std::setw (12) << "kbps"
-                    << std::setw (12) << "delay");
-  NS_LOG_INFO (this << " -----------------------------------------");
-
-  for (uint32_t i = 0; i < n; i++)
-    {
-      throughput = static_cast<double> (m_rxBytes[i] * 8) / 1000.0 / duration;
-      Ptr<NrtvClient> app = m_apps.Get (i)->GetObject<NrtvClient> ();
-      delay = app->GetDelayAverage ();
-      NS_LOG_INFO (this << std::setw (5) << i
-                        << std::setw (12) << m_rxBytes[i]
-                        << std::setw (12) << throughput
-                        << std::setw (12) << delay.GetSeconds ());
-      sumRxBytes += m_rxBytes[i];
-      sumRxPackets += app->GetNumOfRxSlices ();
-      sumRxDelay += app->GetDelaySum ();
-    }
-
-  throughput = static_cast<double> (sumRxBytes * 8) / 1000.0 / duration;
-  delay = Seconds (sumRxDelay.GetSeconds () / static_cast<double> (sumRxPackets));
-  NS_LOG_INFO (this << " -----------------------------------------");
-  NS_LOG_INFO (this << std::setw (5) << "sum"
-                    << std::setw (12) << sumRxBytes
-                    << std::setw (12) << throughput);
-  NS_LOG_INFO (this << std::setw (5) << "avg"
-                    << std::setw (12) << static_cast<double> (sumRxBytes) / n
-                    << std::setw (12) << static_cast<double> (throughput) / n
-                    << std::setw (12) << delay.GetSeconds () / n);
-  NS_LOG_INFO (this << " -----------------------------------------");
-
-  NS_UNUSED (throughput);
-  NS_UNUSED (delay);
-}
-
-void
-NrtvCalculator::RxCallback (std::string context, Ptr<const Packet> packet)
-{
-  uint32_t appId = GetAppId (context);
-  NS_ASSERT (appId < m_rxBytes.size ());
-  m_rxBytes[appId] += packet->GetSize ();
-}
-
-std::string
-NrtvCalculator::GetContext (uint32_t appId)
-{
-  std::ostringstream context;
-  context << appId;
-  return context.str ();
-}
-
-uint32_t
-NrtvCalculator::GetAppId (std::string context)
-{
-  std::stringstream ss (context);
-  uint32_t appId;
-  if (!(ss >> appId))
-    {
-      NS_FATAL_ERROR ("Cannot convert context '" << context << "' to number");
-    }
-  return appId;
-}
 
 
 /**
@@ -173,9 +59,9 @@ NrtvCalculator::GetAppId (std::string context)
 int
 main (int argc, char *argv[])
 {
-  // a workaround to solve weird splitting in lower layer
-  Config::SetDefault ("ns3::TcpL4Protocol::SocketType",
-                      StringValue ("ns3::TcpRfc793"));
+  // a workaround to partially resolve weird splitting in lower layer
+  // Config::SetDefault ("ns3::TcpL4Protocol::SocketType",
+  //                     StringValue ("ns3::TcpRfc793"));
 
   std::string scenario = "simple";
   double duration = 1000;
@@ -235,8 +121,8 @@ main (int argc, char *argv[])
   nrtvHelper.GetServer ().Start (Seconds (1.0));
   nrtvHelper.GetClients ().Start (Seconds (3.0));
 
-  // install calculator
-  NrtvCalculator nrtvCalculator (nrtvHelper.GetClients (), Seconds (duration));
+  // install KPI statistics collector
+  NrtvClientKpiHelper kpi (Seconds (duration), nrtvHelper.GetClients ());
 
   NS_LOG_INFO ("--- sat-nrtv-example ---");
   NS_LOG_INFO ("  Scenario used: " << scenario);
@@ -245,10 +131,8 @@ main (int argc, char *argv[])
 
   Simulator::Stop (Seconds (duration));
   Simulator::Run ();
-
-  nrtvCalculator.Print ();
-
   Simulator::Destroy ();
 
   return 0;
+
 }
