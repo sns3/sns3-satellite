4651bf3c1ba48b0ca67e397b3691c84a7ee104ff
Sami Rantanen <sami.rantanen@magister.fi>
2014-03-15 18:53:09 +0200

    All satellite module examples added to examples-to-run.py script. Trace input/output examples excluded.

--- test/examples-to-run.py
+++ test/examples-to-run.py
@@ -9,13 +9,27 @@
 #
 # See test.py for more information.
 cpp_examples = [
-    ("sat-cbr-example", "True", "True"),
     ("simple-scenario-p2p", "True", "True"),
     ("larger-scenario-p2p", "True", "True"),
+    ("sat-cbr-example", "True", "True"),
+    ("sat-cbr-larger-example", "True", "True"),
+    ("sat-cbr-user-defined-example", "True", "True"),
+    ("sat-environmental-variables-example", "True", "True"),
     ("sat-http-example", "True", "True"),
+    ("sat-link-budget-example", "True", "True"),
+    ("sat-link-result-plot", "True", "True"),
+    ("sat-loo-example", "True", "True"),
+    ("sat-markov-fading-trace-example", "True", "True"),
+    ("sat-markov-logic-example", "True", "True"),
+    ("sat-multi-application-fwd-example", "True", "True"),
+    ("sat-multi-application-rtn-example", "True", "True"),
+    ("sat-multicast-example", "True", "True"),
     ("sat-nrtv-example", "True", "True"),
     ("sat-onoff-example", "True", "True"),
-    ("sat-link-budget-example", "True", "True")
+    ("sat-random-access-crdsa-example", "True", "True"),
+    ("sat-random-access-example", "True", "True"),
+    ("sat-random-access-slotted-aloha-example", "True", "True"),
+    ("sat-rayleigh-example", "True", "True"),
 ]
 
 # A list of Python examples to run in order to ensure that they remain
