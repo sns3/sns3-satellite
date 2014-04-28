943b3e85d0c9220c9b8fb3c87ad6561f6a6045a4
Budiarto Herman <budiarto.herman@magister.fi>
2014-04-28 14:11:40 +0300

    Enabled frame load and waveform usage statistics in TN9 examples

--- examples/sat-dama-sim-tn9.cc
+++ examples/sat-dama-sim-tn9.cc
@@ -246,6 +246,9 @@ main (int argc, char *argv[])
   s->AddPerBeamResourcesGranted (SatStatsHelper::OUTPUT_CDF_FILE);
   s->AddPerBeamResourcesGranted (SatStatsHelper::OUTPUT_CDF_PLOT);
 
+  s->AddPerBeamFrameLoad (SatStatsHelper::OUTPUT_SCALAR_FILE);
+  s->AddPerBeamWaveformUsage (SatStatsHelper::OUTPUT_SCALAR_FILE);
+
   s->AddPerBeamRtnDaPacketError (SatStatsHelper::OUTPUT_SCALAR_FILE);
 
   NS_LOG_INFO("--- sat-dama-sim-tn9 ---");
