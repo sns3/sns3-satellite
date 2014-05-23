d03c005a5c338a423405334250d495c1de9366e5
Budiarto Herman <budiarto.herman@magister.fi>
2014-05-23 13:48:48 +0300

    Fixed attribute typo in RA TN9 simulation

--- examples/sat-ra-sim-tn9.cc
+++ examples/sat-ra-sim-tn9.cc
@@ -94,7 +94,7 @@ main (int argc, char *argv[])
   Config::SetDefault ("ns3::OnOffApplication::OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=" + offTime + "]"));
 
   // Set statistical parameters
-  Config::SetDefault ("ns3::SatStatThroughputHelper::MinValue", DoubleValue (0.0));
+  Config::SetDefault ("ns3::SatStatsThroughputHelper::MinValue", DoubleValue (0.0));
   Config::SetDefault ("ns3::SatStatsThroughputHelper::MaxValue", DoubleValue (5.0));
   Config::SetDefault ("ns3::SatStatsThroughputHelper::BinLength", DoubleValue (0.05));
 
