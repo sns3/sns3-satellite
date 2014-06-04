9532a83e24e2f086fef572f0cf8e0b29539db27f
Budiarto Herman <budiarto.herman@magister.fi>
2014-06-04 13:16:57 +0300

    Draft HTTP simulation for TN9

---
+++ examples/sat-dama-http-sim-tn9.cc
@@ -0,0 +1,251 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/satellite-module.h"
+#include "ns3/traffic-module.h"
+#include "ns3/config-store.h"
+
+
+using namespace ns3;
+
+/**
+* \ingroup satellite
+*
+* \brief Simulation script to run example simulation results with HTTP traffic
+* model. Currently only one beam is simulated with one user and
+* and DAMA configuration.
+*
+* execute command -> ./waf --run "sat-dama-http-sim-tn9 --PrintHelp"
+*/
+
+NS_LOG_COMPONENT_DEFINE ("sat-dama-http-sim-tn9");
+
+int
+main (int argc, char *argv[])
+{
+  // Spot-beam over Finland
+  uint32_t beamId = 18;
+  uint32_t endUsersPerUt (1);
+  uint32_t utsPerBeam (1);
+  uint32_t nccConf (0);
+  uint32_t crTxConf (0);
+
+  double simLength (300.0); // in seconds
+
+  // To read attributes from file
+//  Config::SetDefault ("ns3::ConfigStore::Filename", StringValue ("./src/satellite/examples/tn9-dama-input-attributes.xml"));
+//  Config::SetDefault ("ns3::ConfigStore::Mode", StringValue ("Load"));
+//  Config::SetDefault ("ns3::ConfigStore::FileFormat", StringValue ("Xml"));
+//  ConfigStore inputConfig;
+//  inputConfig.ConfigureDefaults ();
+
+  /**
+   * Attributes:
+   * -----------
+   *
+   * Scenario: 1 beam (beam id = 18)
+   *
+   * Frame configuration:
+   * - 3 frames
+   * - 50 MHz user bandwidth
+   *    - 32 x 0.3125 MHz -> 10 MHz
+   *    - 32 x 0.625 MHz -> 20 MHz
+   *    - 16 x 1.25 MHz -> 20 MHz
+   *
+   * NCC configuration modes
+   * - Conf-0 (static timeslots with ACM off)
+   * - Conf-1 (static timeslots with ACM on)
+   * - Conf-2 scheduling mode (dynamic time slots)
+   * - FCA disabled
+   *
+   * CR transmission modes
+   * - RBDC + periodical control slots
+   * - RBDC + slotted ALOHA
+   * - RBDC + CDRSA (loose RC 0)
+   *
+   * RTN link
+   *   - Constant interference
+   *   - AVI error model
+   *   - ARQ disabled
+   * FWD link
+   *   - ACM disabled
+   *   - Constant interference
+   *   - No error model
+   *   - ARQ disabled
+   *
+   */
+
+  // read command line parameters given by user
+  CommandLine cmd;
+  cmd.AddValue ("nccConf", "NCC configuration", nccConf);
+  cmd.AddValue ("crTxConf", "CR transmission configuration", crTxConf);
+  cmd.Parse (argc, argv);
+
+  // use 5 seconds store time for control messages
+  Config::SetDefault ("ns3::SatBeamHelper::CtrlMsgStoreTimeInRtnLink", TimeValue (Seconds (5)));
+
+  switch (nccConf)
+  {
+    case 0:
+      {
+        Config::SetDefault ("ns3::SatSuperframeConf0::FrameConfigType", StringValue("Config type 0"));
+        Config::SetDefault ("ns3::SatWaveformConf::AcmEnabled", BooleanValue(false));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::MinValue", DoubleValue (0.0));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::MaxValue", DoubleValue (25.0));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::BinLength", DoubleValue (0.1));
+        break;
+      }
+    case 1:
+      {
+        Config::SetDefault ("ns3::SatSuperframeConf0::FrameConfigType", StringValue("Config type 1"));
+        Config::SetDefault ("ns3::SatWaveformConf::AcmEnabled", BooleanValue(true));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::MinValue", DoubleValue (0.0));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::MaxValue", DoubleValue (6.0));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::BinLength", DoubleValue (0.05));
+        break;
+      }
+    case 2:
+      {
+        Config::SetDefault ("ns3::SatSuperframeConf0::FrameConfigType", StringValue("Config type 2"));
+        Config::SetDefault ("ns3::SatWaveformConf::AcmEnabled", BooleanValue(true));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::MinValue", DoubleValue (0.0));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::MaxValue", DoubleValue (6.0));
+        Config::SetDefault ("ns3::SatStatsDelayHelper::BinLength", DoubleValue (0.05));
+        break;
+      }
+    default:
+      {
+        NS_FATAL_ERROR ("Unsupported nccConf: " << nccConf);
+        break;
+      }
+  }
+
+  Config::SetDefault ("ns3::SatStatsThroughputHelper::MinValue", DoubleValue (0.0));
+  Config::SetDefault ("ns3::SatStatsThroughputHelper::MaxValue", DoubleValue (400.0));
+  Config::SetDefault ("ns3::SatStatsThroughputHelper::BinLength", DoubleValue (4.0));
+
+  // RBDC
+  Config::SetDefault ("ns3::SatLowerLayerServiceConf::DaService3_ConstantAssignmentProvided", BooleanValue (false));
+  Config::SetDefault ("ns3::SatLowerLayerServiceConf::DaService3_RbdcAllowed", BooleanValue (true));
+  Config::SetDefault ("ns3::SatLowerLayerServiceConf::DaService3_MinimumServiceRate", UintegerValue (64));
+  Config::SetDefault ("ns3::SatLowerLayerServiceConf::DaService3_VolumeAllowed", BooleanValue (false));
+
+  switch (crTxConf)
+  {
+    // Periodical control slots
+    case 0:
+      {
+        Config::SetDefault ("ns3::SatBeamHelper::RandomAccessModel", EnumValue (SatEnums::RA_MODEL_OFF));
+        Config::SetDefault ("ns3::SatBeamScheduler::ControlSlotsEnabled", BooleanValue (true));
+        break;
+      }
+    // Slotted ALOHA
+    case 1:
+      {
+        Config::SetDefault ("ns3::SatBeamHelper::RandomAccessModel", EnumValue (SatEnums::RA_MODEL_SLOTTED_ALOHA));
+        Config::SetDefault ("ns3::SatBeamScheduler::ControlSlotsEnabled", BooleanValue (false));
+        break;
+      }
+    // CRDSA (loose RC 0)
+    case 2:
+      {
+        Config::SetDefault ("ns3::SatBeamHelper::RandomAccessModel", EnumValue (SatEnums::RA_MODEL_CRDSA));
+        Config::SetDefault ("ns3::SatBeamScheduler::ControlSlotsEnabled", BooleanValue (false));
+        break;
+      }
+    default:
+      {
+        NS_FATAL_ERROR ("Unsupported crTxConf: " << crTxConf);
+        break;
+      }
+  }
+
+  Config::SetDefault ("ns3::SatBeamHelper::CtrlMsgStoreTimeInRtnLink", TimeValue (MilliSeconds (350)));
+
+  // Creating the reference system. Note, currently the satellite module supports
+  // only one reference system, which is named as "Scenario72". The string is utilized
+  // in mapping the scenario to the needed reference system configuration files. Arbitrary
+  // scenario name results in fatal error.
+  std::string scenarioName = "Scenario72";
+
+  Ptr<SatHelper> helper = CreateObject<SatHelper> (scenarioName);
+
+  // create user defined scenario
+  SatBeamUserInfo beamInfo = SatBeamUserInfo (utsPerBeam, endUsersPerUt);
+  std::map<uint32_t, SatBeamUserInfo > beamMap;
+  beamMap[beamId] = beamInfo;
+
+  helper->CreateUserDefinedScenario (beamMap);
+
+  // get users
+  NodeContainer utUsers = helper->GetUtUsers();
+  NodeContainer gwUsers = helper->GetGwUsers();
+
+  /**
+   * Set-up HTTP traffic
+   */
+  HttpHelper httpHelper ("ns3::TcpSocketFactory");
+  httpHelper.InstallUsingIpv4 (gwUsers.Get (0), utUsers);
+  httpHelper.GetServer ().Start (MilliSeconds (1));
+  httpHelper.GetClients ().Start (MilliSeconds (3));
+
+  /**
+   * Set-up statistics
+   */
+  Ptr<SatStatsHelperContainer> s = CreateObject<SatStatsHelperContainer> (helper);
+
+  s->AddPerBeamRtnAppThroughput (SatStatsHelper::OUTPUT_SCATTER_PLOT);
+  s->AddPerBeamRtnAppThroughput (SatStatsHelper::OUTPUT_SCALAR_FILE);
+  s->AddPerBeamRtnDevThroughput (SatStatsHelper::OUTPUT_SCALAR_FILE);
+  s->AddPerBeamRtnMacThroughput (SatStatsHelper::OUTPUT_SCALAR_FILE);
+  s->AddPerBeamRtnPhyThroughput (SatStatsHelper::OUTPUT_SCALAR_FILE);
+
+  s->AddAverageUtUserRtnAppThroughput (SatStatsHelper::OUTPUT_CDF_FILE);
+  s->AddAverageUtUserRtnAppThroughput (SatStatsHelper::OUTPUT_CDF_PLOT);
+
+  s->AddAverageUtUserRtnAppDelay (SatStatsHelper::OUTPUT_CDF_FILE);
+  s->AddAverageUtUserRtnAppDelay (SatStatsHelper::OUTPUT_CDF_PLOT);
+  s->AddPerBeamRtnAppDelay (SatStatsHelper::OUTPUT_CDF_FILE);
+  s->AddPerBeamRtnAppDelay (SatStatsHelper::OUTPUT_CDF_PLOT);
+
+  s->AddAverageUtUserFwdAppThroughput (SatStatsHelper::OUTPUT_CDF_FILE);
+  s->AddAverageUtUserFwdAppThroughput (SatStatsHelper::OUTPUT_CDF_PLOT);
+
+  s->AddAverageUtUserFwdAppDelay (SatStatsHelper::OUTPUT_CDF_FILE);
+  s->AddAverageUtUserFwdAppDelay (SatStatsHelper::OUTPUT_CDF_PLOT);
+  s->AddPerBeamFwdAppDelay (SatStatsHelper::OUTPUT_CDF_FILE);
+  s->AddPerBeamFwdAppDelay (SatStatsHelper::OUTPUT_CDF_PLOT);
+
+  s->AddPerBeamRtnDaPacketError (SatStatsHelper::OUTPUT_SCALAR_FILE);
+  s->AddPerBeamFrameSymbolLoad (SatStatsHelper::OUTPUT_SCALAR_FILE);
+  s->AddPerBeamWaveformUsage (SatStatsHelper::OUTPUT_SCALAR_FILE);
+
+  NS_LOG_INFO("--- sat-dama-http-sim-tn9 ---");
+  NS_LOG_INFO("  Simulation length: " << simLength);
+  NS_LOG_INFO("  Number of UTs: " << utsPerBeam);
+  NS_LOG_INFO("  Number of end users per UT: " << endUsersPerUt);
+  NS_LOG_INFO("  ");
+
+  /**
+   * Store attributes into XML output
+   */
+//  Config::SetDefault ("ns3::ConfigStore::Filename", StringValue ("tn9-dama-output-attributes.xml"));
+//  Config::SetDefault ("ns3::ConfigStore::FileFormat", StringValue ("Xml"));
+//  Config::SetDefault ("ns3::ConfigStore::Mode", StringValue ("Save"));
+//  ConfigStore outputConfig;
+//  outputConfig.ConfigureDefaults ();
+//  outputConfig.ConfigureAttributes ();
+
+  /**
+   * Run simulation
+   */
+  Simulator::Stop (Seconds (simLength));
+  Simulator::Run ();
+
+  Simulator::Destroy ();
+
+  return 0;
+}
+
