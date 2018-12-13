87da4036bf2d0792a8181064bbf0dfa02e8022ae
Joaquin Muguerza <jmuguerza@toulouse.viveris.fr>
2018-12-13 11:20:21 +0100

    Updated copyright

--- stats/satellite-stats-marsala-correlation-helper.h
+++ stats/satellite-stats-marsala-correlation-helper.h
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2014 Magister Solutions
+ * Copyright (c) 2018 CNES
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
