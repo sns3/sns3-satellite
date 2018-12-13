1acce2d3dd9d9f147c3e0ddf18ebc4ed2dc44037
Joaquin Muguerza <jmuguerza@toulouse.viveris.fr>
2018-12-13 16:51:29 +0100

    Updated copyright

--- model/satellite-residual-interference-elimination.h
+++ model/satellite-residual-interference-elimination.h
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2013 Magister Solutions Ltd.
+ * Copyright (c) 2018 CNES
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
