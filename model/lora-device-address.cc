6a31e55b7d905a15ddff19b3be220688ffdb2859
Bastien TAURAN <bastien.tauran@viveris.fr>
2026-07-07 09:53:20 +0200

    Update tests for ns-3.47

--- model/lora-device-address.cc
+++ model/lora-device-address.cc
@@ -177,7 +177,7 @@ LoraDeviceAddress::GetType(void)
 {
     NS_LOG_FUNCTION_NOARGS();
 
-    static uint8_t type = Address::Register();
+    static uint8_t type = Address::Register("LoraDeviceAddress", 4);
     return type;
 }
 
